edbaaec715a356584b4628c0e306d5854e77cb1b
Mike Zbrozek <mzbrozek@cnm.edu>
2022-09-28 13:56:41 -0600

    Second pass with Regular Expression

--- dubstep.js
+++ dubstep.js
@@ -4,16 +4,24 @@
     song = ("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB")
 
     function songDecoder(song) {
+        // return song.split('WUB').filter(word => word.trim()).join(' ')
+        // split the string on WUB
+        // filter out the empty strings
+        // join back together on a space
         const split = song.split('WUB')
         console.log(split)
         const filterWord = split.filter(word => word.trim())
         console.log(filterWord)
         const decoded = filterWord.join(' ')
         return decoded
-        // return song.split('WUB').filter(word => word.trim()).join(' ')
-        // split the string on WUB
-        // filter out the empty strings
-        // join back together on a space
     }
 
     console.log(songDecoder(song))
+
+    //regex
+
+    function songDecoder1(song) {
+        return song.replace(/(WUB)+/g, ' ').trim()
+    }
+
+    console.log(songDecoder1(song))
